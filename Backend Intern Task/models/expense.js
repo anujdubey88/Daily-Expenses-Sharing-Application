552231ecfd22609f85fc13b780a7f56a6c45d0f9
anujdubey88 <anujd1595@gmail.com>
2024-07-27 21:15:55 +0530

    Backend Intern Task

---
+++ Backend Intern Task/models/expense.js
@@ -0,0 +1,30 @@
+// const mongoose = require("mongoose")
+// const schema = mongoose.Schema
+
+// const expenseSchema = new schema({
+//     discription : {
+//         type : String,
+//         required : true
+//     },
+//     amount : {
+//         type : Number,
+//         required : true
+//     },
+//     splitMethod : {
+//         type : String,
+//         required : true
+//     },
+//     participants : [
+//         {
+//             userId: { 
+//                 type: Schema.Types.ObjectId, 
+//                 ref: 'User', 
+//                 required: true 
+//             },
+//             amount: { 
+//                 type: Number, 
+//                 required: true 
+//             }
+//           }
+//     ]
+// })
